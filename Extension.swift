755508c528ad2e3e5b26ec4160c14b240f3797be
abidgurbanov0 <102621027+abidgurbanov0@users.noreply.github.com>
2022-12-24 20:24:25 +0400

    Add files via upload

---
+++ Extension.swift
@@ -0,0 +1,76 @@
+//
+//  Extension.swift
+//  FoodEr
+//
+//  Created by Abid Qurbanov on 14.12.2022.
+//
+
+import UIKit
+
+
+
+
+extension UIView {
+    
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+   
+    
+    @IBInspectable var borderWidth: CGFloat {
+          set {
+              layer.borderWidth = newValue
+          }
+          get {
+              return layer.borderWidth
+          }
+      }
+    
+   
+        @IBInspectable var borderColor: UIColor? {
+            set {
+                layer.borderColor = newValue?.cgColor
+            }
+            get {
+                guard let color = layer.borderColor else {
+                    return nil
+                }
+                return UIColor(cgColor: color)
+            }
+        }
+    
+}
+
+
+
+// dokunma ile klavye kapatma
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+extension UIButton {
+    func preventRepeatedPresses(inNext seconds: Double = 1) {
+        self.isUserInteractionEnabled = false
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
+            self.isUserInteractionEnabled = true
+        }
+    }
+}
+
+
